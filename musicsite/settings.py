3c29a3ab0c16cd18be21ec6a672cd42a30ed5dfb
Aaron Teng <aaronteng@berkeley.edu>
2026-01-22 20:28:49 -0800

    localhost-> db for postgres db

--- musicsite/settings.py
+++ musicsite/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': config('POSTGRES_DB'),
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
